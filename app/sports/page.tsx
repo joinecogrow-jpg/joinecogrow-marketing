cd2a1c5725f1716e6df16d614773a77b9396f00f
Your GitHub Username <your.email@example.com>
2025-10-09 01:37:32 +0100

    add Sports Hub

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,9 +1,3 @@
 ﻿export default function SportsHub() {
-  return (
-    <div className="sports-hub" style={{backgroundColor: "#388E3C"}}>
-      <a href="http://localhost:3000/sports" style={{color: "#7CB342"}}>
-        Sports Hub - Play for the Planet
-      </a>
-    </div>
-  )
+  return <a href="http://localhost:3000/sports">Sports Hub</a>;
 }
